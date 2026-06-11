67729dd9f450b9902c938160a2aeec26e8cb55b7
CamilaVazOliveira <camila.jdaeroporto@gmail.com>
2026-06-11 11:58:16 -0300

    Repositorio React

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter base="/curriculumonline/">
     <App />
   </BrowserRouter>,
 )
